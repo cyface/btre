46c9c29b36db892c76544de9617498bbcaca52a1
Tim White <twhite@fool.com>
2019-02-24 09:17:20 -0700

    Working password and username validations.

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,30 +4,46 @@ from django.db import IntegrityError
 
 
 class RegisterForm(forms.Form):
-    first_name = forms.CharField(required=True)
+    error_css_class = 'error'
+    label_suffix = ''
+    required_css_class = 'required'
+
+    first_name = forms.CharField(required=True, label="First Name")
     first_name.widget.attrs.update({'class': 'form-control'})
 
-    last_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True, label="Last Name")
     last_name.widget.attrs.update({'class': 'form-control'})
 
     username = forms.CharField(required=True)
     username.widget.attrs.update({'class': 'form-control'})
 
-    email = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
     email.widget.attrs.update({'class': 'form-control'})
 
     password = forms.CharField(required=True, widget=forms.PasswordInput)
     password.widget.attrs.update({'class': 'form-control'})
 
-    user = None
+    password_validate = forms.CharField(required=True, widget=forms.PasswordInput, label="Password Again")
+    password_validate.widget.attrs.update({'class': 'form-control'})
 
-    def clean(self):
-        try:
-            user = User.objects.create_user(self.cleaned_data.get('username'), self.cleaned_data.get('email'), self.cleaned_data.get('password'))
-            user.last_name = self.cleaned_data.get('first_name')
-            user.last_name = self.cleaned_data.get('last_name')
-            user.save()
-            self.user = user
-            return True
-        except IntegrityError:
-            return False
+    def clean_password_validate(self):
+        """
+        Validate that the password and password_validate fields match.
+        Since clean_<fieldname> methods are run in order of definition on this class, we check it here rather than on password
+        So that the base type coercions and validations will already have been run for both
+        """
+        if self.cleaned_data.get('password') != self.cleaned_data.get('password_validate'):
+            self.add_error('password', "Passwords Must Match")  # Adding this as a validation fail on password so the error is attached to that field
+        else:
+            return self.cleaned_data.get('password_validate')
+
+    def clean_username(self):
+        """
+        Validate that the username does not already exist
+        """
+        user = User.objects.filter(username=self.cleaned_data.get('username'))
+        if user:
+            self.add_error('username', "This Username Already Exists, Please Choose a Different One")
+        else:
+            return self.cleaned_data.get('username')
+
--- accounts/views.py
+++ accounts/views.py
@@ -28,5 +28,16 @@ class RegisterView(FormView):
     template_name = "accounts/register.html"
 
     def form_valid(self, form):
-        login(self.request, form.user)
+        """
+        Assuming that all the validations ran clean for the RegisterForm,
+        create the user, log them in, and show them their dashboard
+        There is a teeny possibility of a race condition here where someone else signs up with the same username
+        in the microseconds between when we validated that the username was available and when this code runs
+        but the chances are minuscule, and they would just end up seeing the 500 page, worst case. ¯\_(ツ)_/¯
+        """
+        user = User.objects.create_user(form.cleaned_data.get('username'), form.cleaned_data.get('email'), form.cleaned_data.get('password'))
+        user.first_name = form.cleaned_data.get('first_name')
+        user.last_name = form.cleaned_data.get('last_name')
+        user.save()
+        login(self.request, form.cleaned_data.get('user'))
         return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
